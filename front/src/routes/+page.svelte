bcd2a935b4b5b987e94a35749b86f4d1cc40fa97
조유민 <nationmilo@gmail.com>
2024-11-04 01:25:38 +0900

    feat: add global font jp kr

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
+
+<p class="text-2xl font-bold">국가간에 우호관계의 인간은 인간이 폭정과 억압에</p>
+<p>人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは</p>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
