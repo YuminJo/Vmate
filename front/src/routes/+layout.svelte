e37aaeddf955fc2517c07009e96db13adc8384ce
조유민 <nationmilo@gmail.com>
2024-12-12 18:06:30 +0900

    Front (#3)
    * feat: sveltekit front initdata
    
    * feat: add global font jp kr
    
    * feat: flowbite svelte package
    
    * feat: svelte french toast package
    
    * docs: package json
    
    * feat: layout & page list

---
+++ front/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { i18n } from '$lib/i18n';
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<ParaglideJS {i18n}>
+	{@render children()}
+</ParaglideJS>
+
+<header>
+	<a href="/">slog</a>
+	<a href="/member/login">로그인</a>
+	<a href="/member/join">회원가입</a>
+	<a href="/p/list">글</a>
+</header>
+<main>{@render children()}</main>
+<footer>푸터</footer>
---
+++ front/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { Alert } from 'flowbite-svelte';
+	import { InfoCircleSolid } from 'flowbite-svelte-icons';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+
+<p class="text-2xl font-bold">국가간에 우호관계의 인간은 인간이 폭정과 억압에</p>
+<p>人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは</p>
+
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Alert>
+	<span class="font-medium">Default alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
+
+<Alert border>
+	<InfoCircleSolid slot="icon" class="h-5 w-5" />
+	<span class="font-medium">Default alert!</span>
+	Change a few things up and try submitting again.
+</Alert>
